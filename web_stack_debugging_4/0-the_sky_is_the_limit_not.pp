cdc923938db5c0f4564bfe55545b20fdce3ffc2a
Hirwabrian <H.brian@alustudent.com>
2024-08-07 03:53:39 -0700

    5th debugging project

--- web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -4,7 +4,7 @@ exec { 'fix server issue':
   command => 'sed -i "s/15/4096/" /etc/default/nginx',
   path    => '/usr/local/bin/:/bin/'
 }
--> exec { 'nginx-restart':
+exec { 'nginx-restart':
   command => 'nginx restart',
   path    => '/etc/init.d/'
 }
